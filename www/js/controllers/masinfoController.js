b45c0275f7cd8210ffe14893679f51f3f2ce201d
Yi <yi@koko.cr>
2016-05-25 11:21:57 -0600

    mas info view

--- www/js/controllers/masinfoController.js
+++ www/js/controllers/masinfoController.js
@@ -1,8 +1,11 @@
+var DelyciaConstants = require('./../delyciaConstants');
+
 function MasinfoController($scope, $stateParams) {
 	$scope.platilloId = $stateParams.platilloId;
 	$scope.platilloIndex = $stateParams.platilloIndex;
 
-	
+	$scope.platillos = DelyciaConstants.PLATILLOS;
+	$scope.currentPlatillo = $scope.platillos[$scope.platilloIndex];
 }
 
 module.exports = ['$scope', '$stateParams', MasinfoController];
