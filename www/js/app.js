9f5ae4f334b864e1c348a97330808b2f0e9da4a9
Yi <yi@koko.cr>
2016-06-20 14:08:13 -0600

    - Custom header bar. - New Review view. - New Favorite view.

--- www/js/app.js
+++ www/js/app.js
@@ -68,9 +68,9 @@ app.factory('NotificationService', notificationService);
 ////////////////////
 
 app.config(function($stateProvider, $urlRouterProvider, $compileProvider, $ionicConfigProvider, $ionicNativeTransitionsProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):\/\//);
-    $compileProvider.imgSrcSanitizationWhitelist('http://images.contentful.com/');
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|chrome-extension):/);
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|http):\/\//);
+    // $compileProvider.imgSrcSanitizationWhitelist('http://images.contentful.com/');
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|chrome-extension|http):/);
 
     $ionicConfigProvider.scrolling.jsScrolling(false);
 
--- www/js/controllers/reviewController.js
+++ www/js/controllers/reviewController.js
@@ -7,6 +7,16 @@ function ReviewController($scope, $stateParams, contentfulService) {
 					 {id: 3, active: false},
 					 {id: 4, active: false}];
 
+	$scope.testReviews = [{name: 'Valeria Ramirez', comment: 'Estaba deli, vivi Sarchitouille, me encantó. Volvería siempre!', date: '14 junio, 2016', rating: 5},
+						{name: 'Yi Huang', comment: 'No son como los que hacen en Taiwan, pero bueno, se la juegan', date: '9 junio, 2016', rating: 3},
+						{name: 'Esteban Montes', comment: 'Si a Yi no le gustan, que nos invite a unos taiwanes. Estos son solo good.', date: '9 junio, 2016', rating: 4},
+						{name: 'Jimmi Vila', comment: 'Viva Peru. Tomen brasileiros pura tuza! Viva Wendy Sulca!', date: '9 junio, 2016', rating: 5}];
+
+	$scope.testRating = 4.9;
+	$scope.ratingRound = Math.round($scope.testRating);
+
+	$scope.makeRating = false;
+
 	$scope.onHeartClick = function(id)
 	{
 		for(var i = 0, l = $scope.hearts.length; i < l; i++)
@@ -22,20 +32,74 @@ function ReviewController($scope, $stateParams, contentfulService) {
 		}
 	}
 
-	// if(contentfulService.mainDishes.length === 0)
-	// {
-	// 	$scope.currentPlatillo = {id:'0', src:'', title:'', restaurant:'', price:0, rating:0, distance: '', status: ''};
+	$scope.getHeartClass = function(index, rating)
+	{
+		if(index < rating)
+		{
+			return 'ion-heart active';
+		}
+		else
+		{
+			return 'ion-heart inactive';
+		}
+	};
 
-	// }
-	// else
-	// {
-	// 	$scope.currentPlatillo = contentfulService.getDishJson($scope.platilloId);
-	// }
+	$scope.getReviewColor = function(index)
+	{
+		var lastDigit = (index+1) % 10;
+		switch (lastDigit)
+		{
+			case 1:
+			case 6:
+				return 'decoration1';
+			case 2:
+			case 7:
+				return 'decoration2';
+			case 3:
+			case 8:
+				return 'decoration3';
+			case 4:
+			case 9:
+				return 'decoration4';
+			case 5:
+			case 0:
+				return 'decoration5';
+		}
+	};
+
+	$scope.onTastedButtonClick = function()
+	{
+		if(!$scope.makeRating)
+		{
+			$scope.makeRating = true;
+		}
+	};
+
+	$scope.onReturnButtonClick = function()
+	{
+		$scope.makeRating = false;
+	};
+
+	$scope.onSubmitButtonClick = function()
+	{
+		$scope.makeRating = false;
+	};
+
+
+	if(contentfulService.mainDishes.length === 0)
+	{
+		$scope.currentPlatillo = {id:'0', src:'', title:'', restaurant:'', price:0, rating:0, distance: '', status: ''};
+
+	}
+	else
+	{
+		$scope.currentPlatillo = contentfulService.getDishJson($scope.platilloId);
+	}
 	
 
-	// $scope.$on('ready',function(data,items){
-	// 	$scope.currentPlatillo = contentfulService.getDishJson($scope.platilloId);
-	// });
+	$scope.$on('ready',function(data,items){
+		$scope.currentPlatillo = contentfulService.getDishJson($scope.platilloId);
+	});
 }
 
 module.exports = ['$scope', '$stateParams', 'ContentfulService', ReviewController];
