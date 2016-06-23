4de0d648b169dea16e8ac7a6ae0a8085e18d944a
Yi <yi@koko.cr>
2016-06-23 12:17:25 -0600

    add date time picker

--- www/js/controllers/favoritesController.js
+++ www/js/controllers/favoritesController.js
@@ -5,6 +5,25 @@ function FavoritesController($scope, $stateParams) {
 
 	$scope.platillos = DelyciaConstants.PLATILLOS;
 	$scope.currentPlatillo = $scope.platillos[$scope.platilloId];
+
+	var options = {
+    date: new Date(),
+    mode: 'datetime'
+	};
+
+	function onSuccess(date) {
+	    alert('Selected date: ' + date);
+	}
+
+	function onError(error) { // Android only
+	    alert('Error: ' + error);
+	}
+
+	$scope.showDatePicker = function()
+	{
+		options.date = new Date();
+		datePicker.show(options, onSuccess, onError);
+	}
 }
 
 module.exports = ['$scope', '$stateParams', FavoritesController];
